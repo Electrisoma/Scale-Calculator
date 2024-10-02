6ca290b38600dabff59bb6c2f46b251f51b39692
Electrisoma <electrisoma@gmail.com>
2024-10-01 23:55:03 -0500

    Update DoubleHarmonicMinor.py

--- expansions/DoubleHarmonicMinor.py
+++ expansions/DoubleHarmonicMinor.py
@@ -26,20 +26,26 @@ def scale():
   print(f"{colors.LG}5.Double Harmonic Major{colors.END}")
   print(f"{colors.LG}6.Lydian #2 #6{colors.END}")
   print(f"{colors.LG}7.Ultraphrygian{colors.END}")
+
   while True:
     choice = input(f"{colors.LP}>Enter scale: {colors.END}")
-    if choice == 'b':
+
+    if choice == 'b': #back button
       back()
       break
+
     firstPrompt = input(f"{colors.R}>Sharps or flats(1/2): {colors.END}")
+
     if firstPrompt in ['1','2']:
       if firstPrompt == '1':
         scale = scaleA
       elif firstPrompt == '2':
         scale = scaleB 
-    if firstPrompt == 'b':
+
+    if firstPrompt == 'b': #back button
       back()
       break
+
     if choice in ['1','2','3','4','5','6','7']:
         try:
           if choice == '1':
@@ -56,19 +62,23 @@ def scale():
             print(f"{colors.LG}{colors.BOLD}>>Lydian #2 #6{colors.END}")
           if choice == '7':
             print(f"{colors.LG}{colors.BOLD}>>Ultraphrygian{colors.END}")
+
           if firstPrompt == '1':
             inp = int(input(f"{colors.R}>Enter number of sharps(>2): {colors.END}"))
           elif firstPrompt == '2':
             inp = int(input(f"{colors.R}>Enter number of flats(>2): {colors.END}"))
-          if inp == 0:
+
+          if inp == 0: #preventing some weird stuff
             inp = 1
           if inp & 1: #circle of fifths calculator
             flats = int(inp) + 1
           else:
             flats = int(inp) - 3
+
         except ValueError:
              print(invalid)
              continue
+
         if choice == '1': #double harm minor
           def shiftscale(scale,flats):
             scaleshifter = deque(scale)
@@ -80,6 +90,7 @@ def scale():
             newscale.pop(3)
             newscale.pop(6)
             return newscale        
+
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
           print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Double Harmonic Minor" + f"{colors.END}")
         elif choice == '2': #loc dom #6
@@ -93,6 +104,7 @@ def scale():
             newscale.pop(5)
             newscale.pop(5)
             return newscale 
+
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
           print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Locrian Dominant #6" + f"{colors.END}")
         elif choice == '3': #ion #2 #5
@@ -106,6 +118,7 @@ def scale():
             newscale.pop(4)
             newscale.pop(6)
             return newscale 
+
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
           print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Ionian #2 #5" + f"{colors.END}")
         elif choice == '4': #loc bb3 bb7
@@ -119,6 +132,7 @@ def scale():
             newscale.pop(3)
             newscale.pop(5)
             return newscale 
+
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
           print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Locrian bb3 bb7" + f"{colors.END}")
         elif choice == '5': #Double harm major
@@ -132,8 +146,9 @@ def scale():
             newscale.pop(4)
             newscale.pop(6)
             return newscale 
+
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
-          print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Double Harmonic Major" + f"{colors.END}")          
+          print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Double Harmonic Major" + f"{colors.END}")      
         elif choice == '6': #lydian #2 #6
           def shiftscale(scale,flats):
             scaleshifter = deque(scale)
@@ -144,7 +159,8 @@ def scale():
             newscale.pop(1)
             newscale.pop(3)
             newscale.pop(5)
-            return newscale 
+            return newscale
+ 
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
           print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Lydian #2 #6" + f"{colors.END}")          
         elif choice == '7': #ultraphrygian
@@ -158,6 +174,7 @@ def scale():
             newscale.pop(4)
             newscale.pop(4)
             return newscale 
+
           print(f"{colors.Y}" + str(shiftscale(scale,flats)) + f"{colors.END}")
           print(f"{colors.LG}" + shiftscale(scale,flats)[0] + " " + "Ultraphrygian" + f"{colors.END}")   
     else:
